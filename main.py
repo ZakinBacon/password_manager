4951bd77d12fcd9383a299c2d5789acc95e6e52e
ZakinBacon <zakin68@gmail.com>
2023-06-06 10:07:22 -0700

    Added comments on everything

--- main.py
+++ main.py
@@ -3,17 +3,19 @@ from tkinter import messagebox
 import random
 import pyperclip
 
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
     password_entry.delete(0, END)
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
     nr_letters = random.randint(8, 10)
     nr_symbols = random.randint(2, 4)
     nr_numbers = random.randint(2, 4)
-
 
     password_letters = [random.choice(letters) for _ in range(nr_letters)]
     password_symbols = [random.choice(symbols) for _ in range(nr_symbols)]
@@ -23,37 +25,43 @@ def generate_password():
 
     random.shuffle(password_list)
 
-    password = "".join(password_list)
+    password = "".join(password_list)  # uses Join to add the list to a string
 
-    password_entry.insert(0, password) # replaces the entry with the password generated
-    pyperclip.copy(password) # saves the password to your clipboard
+    password_entry.insert(0, password)  # replaces the entry with the password generated
+    pyperclip.copy(password)  # saves the password to your clipboard
+
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_info():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
 
+    # Checks if any of the boxes are empty and returns an error
     if len(website) == 0 or len(email) == 0 or len(password) == 0:
         messagebox.showerror(title="Oops", message="Please dont leave any fields empty")
         return
 
-    is_ok = messagebox.askokcancel(title=website, message= f"These are the details entered:"
-                                                   f" \nEmail: {email} \nPassword {password} \nIs it ok to save?")
+    # Pop up box to confirm the info before saving it
+    is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered:"
+                                                          f" \nEmail: {email} \nPassword {password} \nIs it ok to save?")
 
     if is_ok:
         # saves info to txt file
         with open("password_manager.txt", "a") as data_file:
             data_file.write(f"{website} | {email} | {password} \n")
+            # Puts the Entries back to empty
             website_entry.delete(0, END)
             password_entry.delete(0, END)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
-
+# Creates window
 window = Tk()
 window.title("Password Manager")
 window.config(padx=20, pady=20)
 
+# Creates the canvas for the image
 canvas = Canvas(width=200, height=200)
 logo = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo)
@@ -70,23 +78,22 @@ email_title.grid(column=0, row=2, padx=20, pady=20, sticky='E')
 password_title = Label(text="Password:")
 password_title.grid(column=0, row=3, padx=20, pady=20, sticky='E')
 
-# -------------entries for info-----------------#
+# -------------Entries for info-----------------#
 
 website_entry = Entry(width=70)
 website_entry.grid(column=1, row=1, columnspan=2, padx=20, pady=20, sticky='NW')
-website_entry.focus()
+website_entry.focus() # Auto selects this entry when the program is run
 
 email_entry = Entry(width=70)
 email_entry.grid(column=1, row=2, columnspan=2, padx=20, pady=20, sticky='NW')
-email_entry.insert(END, "Zach@gmail.com")
+email_entry.insert(END, "Zach@gmail.com") # Sets the default to be this email
 
 password_entry = Entry(width=50)
 password_entry.grid(column=1, row=3, padx=20, pady=20, sticky='NW')
 
-# --------------Button-----------------#
+# --------------Buttons-----------------#
 generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(column=2, row=3)
-
 
 add_button = Button(text="Add", width=60, command=save_info)
 add_button.grid(column=1, row=4, columnspan=2)
